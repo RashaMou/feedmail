de816cffef00aea5b0fe1553a31dddf6ff2b8abe
Rasha Moumneh <rasha@fastmailteam.com>
2024-05-26 21:14:42 -0400

    cli: add, list, delete feed methods

--- feedmail/cli.py
+++ feedmail/cli.py
@@ -1,5 +1,6 @@
 import typer
-from db import init_db
+from db import Feed, get_db_session, init_db
+from sqlalchemy import select
 
 app = typer.Typer()
 
@@ -9,7 +10,17 @@ def add(feed: str, name: str):
     """
     Add a feed to the database
     """
-    pass
+    engine = get_engine()
+
+    with get_session(engine) as session:
+        try:
+            new_feed = Feed(url=feed, name=name)
+            typer.echo(f"Adding feed: {feed} with name: {name}")
+            session.add(new_feed)
+            session.commit()
+        except Exception as e:
+            typer.echo(f"Error adding feed: {e}")
+            session.rollback()
 
 
 @app.command()
@@ -17,7 +28,17 @@ def remove(feed_name: str):
     """
     Remove a feed from the database by name
     """
-    pass
+
+    with get_db_session() as session:
+        feed_to_delete = (
+            session.query(Feed).filter(Feed.name == feed_name).one_or_none()
+        )
+        try:
+            session.delete(feed_to_delete)
+            session.commit()
+        except Exception as e:
+            typer.echo(f"Error deleting feed: {e}")
+            session.rollback()
 
 
 @app.command()
@@ -25,7 +46,14 @@ def list():
     """
     List all the feeds by name and url
     """
-    pass
+    with get_db_session() as session:
+        stmt = select(Feed.name, Feed.url)
+        result = session.execute(stmt)
+        feeds = result.all()
+
+        for name, url in feeds:
+            typer.echo(f"Feed name: {name}\nUrl: {url}")
+            return feeds
 
 
 @app.command()
